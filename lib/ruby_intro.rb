2b380ccd1a893f4fb8643192b0fb925a051b568e
CathalRyan96 <cathalryan48@gmail.com>
2019-02-06 09:43:59 +0000

    hw1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -2,13 +2,26 @@
 
 # Part 1
 
-def sum arr
-  # YOUR CODE HERE
+def sum array
+  return 0 if array.empty?
+  array.inject 0, :+
+  
 end
 
-def max_2_sum arr
-  # YOUR CODE HERE
+def max_2_sum array
+  if array.empty?
+    arr_sum = 0
+  elsif arr.length == 1
+  arr_sum = arr[0]
+else
+  arr1 = arr.sort { |x,y| y <=> x}
+  arr_sum = arr1[0] + arr1[1]
+  
 end
+
+return arr_sum
+end
+
 
 def sum_to_n? arr, n
   # YOUR CODE HERE
@@ -17,11 +30,11 @@ end
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+   "Hello, " + name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  s[0] =~ /[bcdfghjklmnprstvwxyz]+/i
 end
 
 def binary_multiple_of_4? s
@@ -31,5 +44,12 @@ end
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  def initialize (isbn, price)
+    @isbn =isbn
+    @price = price
+  end
+  
+  def price_as_string
+    '%.2f'%[@price]
+  end
 end
